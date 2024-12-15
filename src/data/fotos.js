4116dfd2467318c66d8e9702fba873a4d3e1b748
Viajaum <contato.viajaum@gmail.com>
2024-12-15 13:32:16 -0300

    Initial commit for the project

---
+++ src/data/fotos.js
@@ -0,0 +1,29 @@
+// src/data/fotos.js
+export const fotos = [
+    {
+      id: 1,
+      slug: 'foto-1',
+      imagem: 'foto-1.jpg', // Imagem armazenada na pasta 'public/images/'
+    },
+    {
+      id: 2,
+      slug: 'foto-2',
+      imagem: 'foto-2.jpg',
+    },
+    {
+        id: 3,
+        slug: 'foto-3',
+        imagem: 'foto-3.jpg',
+      },  
+      {
+        id: 4,
+        slug: 'foto-4',
+        imagem: 'foto-4.jpg',
+      },
+      {
+        id: 5,
+        slug: 'foto-5',
+        imagem: 'foto-5.jpg',
+      },
+    ];
+  
---
+++ src/data/noticias.js
@@ -0,0 +1,19 @@
+export const noticias = [
+    {
+      slug: "noticia-1",
+      titulo: "Título da Notícia 1",
+      resumo: "Resumo breve da notícia 1.",
+      conteudo: "Conteúdo completo da notícia 1.",
+      imagem: '/images/noticia-1.jpg',
+    },
+    {
+      slug: "noticia-2",
+      titulo: "Título da Notícia 2",
+      resumo: "Resumo breve da notícia 2.",
+      conteudo: "Conteúdo completo da notícia 2.",
+      imagem: '/images/noticia-2.jpg',
+
+    },
+    // Adicione mais notícias aqui
+  ];
+  
